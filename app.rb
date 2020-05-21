ceeb9f60ebad073d392a2c30386dae1faeb619c4
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-21 02:56:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,10 +12,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    # "#{params[:phrase]*params[:number].to_i}"
-    # (params[:number].to_i).times do
-      "#{params[:phrase]}"
-    # end
+    "#{params[:phrase]*params[:number].to_i}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
